c57aeb44521f0ba5d7378308762baf974479f6d7
Sean McCann <smccann@athium.com>
2019-07-11 09:56:59 +0100

    extra messages

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        User::truncate();
+
+
+        $adminRole= Role::where('name', 'admin')->first();
+        $authorRole= Role::where('name', 'author')->first();
+        $userRole= Role::where('name', 'user')->first();
+
+
+        $admin= User::create([
+
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+
+
+        ]);
+
+
+        $author= User::create([
+
+            'name' => 'user',
+            'email' => 'author@author.com',
+            'password' => bcrypt('password')
+
+
+        ]);
+
+        $user= User::create([
+
+            'name' => 'Admin',
+            'email' => '$user@user.com',
+            'password' => bcrypt('password')
+
+
+        ]);
+
+
+        $admin->roles()->attach($adminRole);
+        $author->roles()->attach($authorRole);
+        $user->roles()->attach($userRole);
+
+    }
+}
